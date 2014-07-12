8044209b33ad44bccf5497878b02ae1080145385
Mike Land <mikepland@gmail.com>
2014-07-12 10:59:31 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MicardRails4::Application.config.secret_token = 'f17a57bd6f6c573703371bf0b0220f53adc4b270d96de7480116963329f5ae3a52ddebfcc4ab8d927a0eebf445e620a62b732f9c1a66ece192ed4b9d80058da5'
